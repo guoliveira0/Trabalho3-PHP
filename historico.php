d28f9422094e6d74e74a6af8e79634da789f1463
guoliveira0 <luizgustavo160206@gmail.com>
2023-09-12 09:22:25 -0300

    linkando css

--- historico.php
+++ historico.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recuperar</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header><h2>Histórico</h2></header>
